4379e9aec87d7a5826789f0eb287b1249ad46309
LowIQCoder <marselberheev@mail.ru>
2024-04-02 09:55:49 +0300

    jjjjhggyj

---
+++ Patterns/patterns/src/main/java/patterns/Decorator.java
@@ -0,0 +1,20 @@
+package patterns;
+
+/*
+ *      ████    ██████  ██████  ██████  ██████  ██████  ██████  ██████  ██████
+ *      ██  ██  ██      ██      ██  ██  ██  ██  ██  ██    ██    ██  ██  ██  ██
+ *      ██  ██  ██████  ██      ██  ██  ██████  ██████    ██    ██  ██  ██████
+ *      ██  ██  ██      ██      ██  ██  ████    ██  ██    ██    ██  ██  ████
+ *      ████    ██████  ██████  ██████  ██  ██  ██  ██    ██    ██████  ██  ██
+ * 
+ *   Allows to change the bahovour of the object by warping them in other objects
+ *                               (Structural pattern)
+ *   When to use:
+ *   - Behavour of the object can change dynamically
+ *   - Hard to extend bahavour of the object using the inheritance
+ */
+public class Decorator {
+    
+}
+
+
