b5509ab35f7be455b4c7137cf92c6678bc3c5bd6
LowIQCoder <marselberheev@mail.ru>
2024-04-02 09:18:27 +0300

    Better

--- Patterns/patterns/src/main/java/lc.java
+++ Patterns/patterns/src/main/java/lc.java
@@ -10,7 +10,6 @@ public class lc {
         Graph<Integer> graph = new Graph<Integer>();
         for (int i = 0; i < n; i++) {
             Vertex<Integer> v = new Vertex<Integer>(i);
-            graph.addVertex(v);
         }
         for (int i = 0; i < n; i++) {
             for (int q = 0; q < n; q++) {
@@ -26,74 +25,64 @@ public class lc {
 }
 
 class Graph<T extends Comparable<T>> {
-    ArrayList<Vertex<T>> vertices;
-    ArrayList<Edge<T>> edges;
-    public void addVertex(Vertex<T> v) {
-        this.vertices.add(v);
-    }
-    public void removeVertex(Vertex<T> v) {
-        this.vertices.remove(v);
+    Vertex<T> rootVertex;
+    public void addVertex(T key) {
+        Vertex<T> newVertex = new Vertex<>(key);
+        if (this.rootVertex == null) {
+            this.rootVertex = newVertex;
+        }
+
+        this.rootVertex.prevVertex = newVertex;
+        newVertex.nextVertex = this.rootVertex;
+        this.rootVertex = newVertex;
     }
     public void addEdge(T k1, T k2) {
         Vertex<T> v1 = null;
         Vertex<T> v2 = null;
-        for (int i = 0; i < vertices.size(); i++) {
-            if (vertices.get(i).getKey() == k1) {
-                v1 = vertices.get(i);
+        Vertex<T> next = this.rootVertex;
+        while (next != null) {
+            if (next.getKey() == k1) {
+                v1 = next;
             }
-            if (vertices.get(i).getKey() == k2) {
-                v2 = vertices.get(i);
+            if (next.getKey() == k2) {
+                v2 = next;
             }
         }
-        if (v1 == null || v2 == null) {
-            System.err.println("No vertex exists");
-        }
+
         Edge<T> e = new Edge<T>(v1, v2);
         v1.addEdge(e);
         v2.addEdge(e);
-        this.edges.add(e);
-    }
-    public void removeEdge(Vertex<T> v1, Vertex<T> v2) {
-        return;
     }
     public void MarselBerheev_dfs(T start) {
-        Vertex<T> strt = null;
-        for (int i = 0; i < vertices.size(); i++) {
-            if (vertices.get(i).getKey() == strt) {
-                strt = vertices.get(i);
-            }
-        }
-        if (strt == null) {
-            System.err.println("No edge exists");
-        }
-        
+                
     }
 }
 class Edge<T extends Comparable<T>> {
-    Vertex<T> first;
-    Vertex<T> second;
+    Vertex<T> from;
+    Vertex<T> to;
+    Edge<T> nextEdge;
+    Edge<T> prevEdge;
     Edge(Vertex<T> v1, Vertex<T> v2) {
-        this.first = v1;
-        this.second = v2;
+        this.from = v1;
+        this.to = v2;
     }
 }
 class Vertex<T extends Comparable<T>> {
     private T value;
-    List<Edge<T>> adjacencyList;
+    Edge<T> rootEdge;
+    Vertex<T> nextVertex;
+    Vertex<T> prevVertex;
     Vertex(T key) {
         this.value = key;
     }
-    public void setValue(T v) {
-        this.value = v;
-    }
-    public T getValue() {
-        return this.value;
-    }
     public void addEdge(Edge<T> e) {
-        this.adjacencyList.add(e);
-    }
-    public void removeEdge(Edge<T> e) {
-        this.adjacencyList.remove(e);
+        if (this.rootEdge == null) {
+            this.rootEdge = e;
+        }
+
+        this.rootEdge.prevEdge = e;
+        e.nextEdge = this.rootEdge;
+        this.rootEdge = e;
     }
     public T getKey() {
         return this.value;
